35ef25ccbd761b3f8b6b90858804f72f7d67b48a
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-01-23 20:53:27 +0100

    Enhance unit converter with more conversions and security
    Added additional unit conversion constants and password protection for access.

--- Uconvert.c
+++ Uconvert.c
@@ -1,11 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 /*Main Source File: Unit Converter*/
 int main() {
  /*Conversion from one unit to another*/
-  const double G_TO_KG = 1;
-  const double KG_TO_G= 1000;
-  const double M_TO_KM = 1;
-  const double KM_TO_M = 1000;
-  const double
+
+  char password [50];
+  int choice;
+  int operands;
+  const double answer;
+  const double question;
+  const double G_TO_KG = 1; //grams to kilograms
+  const double KG_TO_G= 1000; //kilograms to grams
+  const double M_TO_KM = 1; //metre to kilometre
+  const double KM_TO_M = 1000;//kilometre to metre
+  const double CM_TO_MM = 10; //centimetre to millimetre
+  const double MM_TO_CM = 0.1; //millimetre to centimetre
+  const double MIN_TO_SEC = 60; //minute to second
+  const double SEC_TO_MIN = 0.0166667; //second to minute
+  const double CELS_TO_KELV = 273.15; //celsius to kelvin
+  const double KELV_TO_CELS = -273.15; //kelvin to celsius
+ printf("\t******************************************************\n");
+ printf("\t* Enter Password: ");
+ printf("%49s", password);
+ printf("\t******************************************************\n");
+ printf("\n");
+ 
+ if(strcmp(password, "Lawrex+17") == 0) {
+ printf("\t******************************************************\n");
+ printf("\t*                  Access Granted                    *\n");
+ printf("\t******************************************************\n");
+ printf("\n");
+ printf("\t******************************************************\n");
+ printf("\t*   ******    *    **   *    ******    ******     *  *\n");
+ printf("\t*   *         *    * *  *    *    *    *******    *  *\n");
+ printf("\t*   *         *    *  * *    ******    ******     *  *\n");
+ printf("\t*   ******    *    *   **    *    *    *     *    *  *\n");
+ printf("\t******************************************************\n");
+ printf("\n");
+ printf("\t******************************************************\n");
+ printf("\t*Enter 1 for an operand ")
+ printf("\t******************************************************\n");
+  if(operands <= 0 || operands >= 3) {}
+  else if(operand == )
+ }
   
+else {
+printf("\t******************************************************\n");
+printf("\t*                  Access Denied                     *\n");
+printf("\t******************************************************\n");
+return 0;
+ }
+return 0;
 }
