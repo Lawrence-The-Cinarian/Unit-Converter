2915055c0b1c412752bc46eae31ade35cdb80e72
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-01-23 18:26:20 +0100

    Add unit conversion constants in Uconvert.c
    Initial implementation of unit conversion constants.

---
+++ Uconvert.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*Main Source File: Unit Converter*/
+int main() {
+ /*Conversion from one unit to another*/
+  const double G_TO_KG = 1;
+  const double KG_TO_G= 1000;
+  const double M_TO_KM = 1;
+  const double KM_TO_M = 1000;
+  const double
+  
+}
